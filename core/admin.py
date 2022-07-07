28502a6eddaaf7bca00f206a2336b673281222fb
selminho <henry3info3@gmail.com>
2022-07-07 09:13:42 -0300

    init

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from core.models import Categoria, Editora, Livro, Autor
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Autor)
